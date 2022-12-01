089fac95aa1a6e927aaf7bc53a268570db72e581
Marcus Vinicius <marcusviniciusfabr@gmail.com>
2022-12-01 08:11:20 -0300

    feat: add configuration for api gateway logging

--- lib/e_commerce_api-stack.ts
+++ lib/e_commerce_api-stack.ts
@@ -1,8 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import { Construct } from 'constructs';
-// import * as cloudwatchlogs from 'aws-cdk-lib/aws-logs';
 import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as cloudwatchlogs from 'aws-cdk-lib/aws-logs';
+import { Construct } from 'constructs';
 
 interface IEcommerceApiStackProps extends cdk.StackProps {
   productFetchHandler: lambdaNodeJS.NodejsFunction;
@@ -12,8 +12,24 @@ export class ECommerceApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: IEcommerceApiStackProps) {
     super(scope, id, props);
 
+    const logGroup = new cloudwatchlogs.LogGroup(this, 'ECommerceApiLogs');
+
     const api = new apigateway.RestApi(this, 'ECommerceApi', {
-      restApiName: 'ECommerceApi'
+      restApiName: 'ECommerceApi',
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true, // Configuração custosa
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true // Configuração custosa
+        })
+      }
     });
 
     const productsFetchIntegration = new apigateway.LambdaIntegration(props.productFetchHandler);
