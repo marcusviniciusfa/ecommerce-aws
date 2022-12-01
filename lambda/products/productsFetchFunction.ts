e9043030792ab9e404559f87fee6717bf76ef38f
Marcus Vinicius <marcusviniciusfabr@gmail.com>
2022-11-30 22:48:39 -0300

    feat: add log for lambdaRequestId and apiRequestId

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,6 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+  const lambdaRequestId = context.awsRequestId;
+  const apiRequestId = event.requestContext.requestId;
+  console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
+
   if (event.resource === '/products') {
     if (event.httpMethod === 'GET') {
       console.log('GET');
